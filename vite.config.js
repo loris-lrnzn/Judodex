6620f61c79b9b8e7a331e2efdbc8804fcd3e1ef2
Loris LORENZINI <lorislorenzini@outlook.com>
2025-06-13 18:55:06 +0200

    test 3

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://sae401-25.mmi-stdie.fr/lorisl/wp-json/judodex/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    base: './', // essentiel pour Netlify ou hébergement dans un sous-dossier
+    server: {
+      proxy: isProduction ? undefined : {
+        '/api': {
+          target: 'https://sae401-25.mmi-stdie.fr/lorisl/wp-json/judodex/v1',
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
